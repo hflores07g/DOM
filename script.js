6059513a5e1f7b0e47bb8d969632e09dabaa69ae
Henry Flores <hflores07@gmail.com>
2023-04-26 00:25:18 -0500

    corregido #id

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const h1=document.querySelector('h1');
+const h1=document.querySelector('#h1');
 const input1=document.querySelector('#calculo1');
 const input2=document.querySelector('#calculo2');
 const btn=document.querySelector('#btnCalcular');
-const pResult=document.querySelector('#result');
+const pResult=document.querySelector('#resultado');
 
 btn.addEventListener('click',btnOnClick);
 
